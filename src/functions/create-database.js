1a4b31ce9e91c3dfff81d460ba2c2af18ba6f0e9
Matt Curtis <matt.curtis@open-xchange.com>
2020-09-04 14:03:44 -0400

    lets give this a try

--- src/functions/create-database.js
+++ src/functions/create-database.js
@@ -1,40 +1,40 @@
 exports.handler = function(event, context, callback) {
   const fetch = require('node-fetch')
-  return JSON.stringify(JSON.parse(event.body))
-  // const id = event.id
+  const data = JSON.parse(event.body)
+  const id = data.id
 
-  // if (!id) {
-  //   throw new Error('id was not specified in payload')
-  // }
+  if (!id) {
+    throw new Error('id was not specified in payload')
+  }
 
-  // const dbUrl = process.env.DB_URL,
-  //   dbUser = process.env.DB_USER,
-  //   dbPass = process.env.DB_PASS
+  const dbUrl = process.env.DB_URL,
+    dbUser = process.env.DB_USER,
+    dbPass = process.env.DB_PASS
 
-  // // make sure things are defined
-  // if (!dbUrl && !dbUser && !dbPass) throw new Error ('database credentials have not been stored/fetched correctly.')
+  // make sure things are defined
+  if (!dbUrl && !dbUser && !dbPass) throw new Error ('database credentials have not been stored/fetched correctly.')
 
-  // // assemble the full db url with credentials, if they already aren't part of it
-  // if (!dbUrl.includes(dbUser) && !dbUrl.includes(dbPass)) {
-  //   const baseUrl = dbUrl.slice(dbUrl.lastIndexOf('://') + 3)
-  //   dbUrl = dbUrl.replace(':\/\/', `://${dbUser}:${dbPass}@${baseUrl}`)
-  // }
+  // assemble the full db url with credentials, if they already aren't part of it
+  if (!dbUrl.includes(dbUser) && !dbUrl.includes(dbPass)) {
+    const baseUrl = dbUrl.slice(dbUrl.lastIndexOf('://') + 3)
+    dbUrl = dbUrl.replace(':\/\/', `://${dbUser}:${dbPass}@${baseUrl}`)
+  }
 
-  // // tack the id on, as defined in CouchDB API
-  // dbUrl = dbUrl + '/' + id
+  // tack the id on, as defined in CouchDB API
+  dbUrl = dbUrl + '/' + id
 
-  // // call put method to create the new database
-  // const response = await fetch(dbUrl, {
-  //   method: 'PUT'
-  // })
+  // call put method to create the new database
+  const response = await fetch(dbUrl, {
+    method: 'PUT'
+  })
   
-  // if (response.ok) return id
+  if (response.ok) return id
 
-  // switch (response.status) {
-  //   case 412:
-  //     // already created, just return the id
-  //     return id
-  //   default:
-  //     throw new Error(response.statusText)
-  // }
+  switch (response.status) {
+    case 412:
+      // already created, just return the id
+      return id
+    default:
+      throw new Error(response.statusText)
+  }
 }
