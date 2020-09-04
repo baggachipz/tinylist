08fa9a5567ef40629dcac919edd223d711d325a2
Matt Curtis <matt.curtis@open-xchange.com>
2020-09-04 10:47:44 -0400

    Test vars data

--- src/functions/create-database.js
+++ src/functions/create-database.js
@@ -1,6 +1,6 @@
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   const fetch = require('node-fetch')
-  return event
+  return { event, context }
   const id = event.id
 
   if (!id) {
